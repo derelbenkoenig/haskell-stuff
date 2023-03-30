fa6d5a02c651056838462733e56091b18ef32636
Robert Benson <rbenson@bandwidth.com>
2023-03-30 14:29:32 -0400

    function to fill random cells; use undefined for initial focus; rule30 is redundant

--- cellular-automata/src/CellularAutomata.hs
+++ cellular-automata/src/CellularAutomata.hs
@@ -4,6 +4,7 @@ module CellularAutomata where
 
 import Control.Comonad
 import Control.Comonad.Representable.Store
+import Control.Monad.Random
 import Data.Bool
 import Data.Functor.Identity
 import Data.Functor.Rep
@@ -11,6 +12,7 @@ import Data.Distributive
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import qualified Data.Vector as V
+import Data.Word (Word8)
 
 -- using constant length,
 -- maybe at some point make it a type parameter of kind Nat
@@ -30,12 +32,13 @@ vecToText = T.pack . V.foldr' (:) ""
 boolVecToInt :: V.Vector Bool -> Int
 boolVecToInt = V.foldl' (\n digit -> n * 2 + fromEnum digit) 0
 
-intToBoolVec :: Int -> V.Vector Bool
-intToBoolVec 0 = V.empty
-intToBoolVec i
+integralToBoolVec :: Integral a => a -> V.Vector Bool
+integralToBoolVec 0 = V.empty
+integralToBoolVec i
   | i < 0 = error "cannot convert negative number to bits"
-  | otherwise = let (halfI, bit) = i `divMod` 2
-                 in intToBoolVec halfI V.++ V.singleton (toEnum bit)
+  | otherwise =
+      let (halfI, bit) = i `divMod` 2
+       in integralToBoolVec halfI V.++ V.singleton (toEnum $ fromIntegral bit)
 
 fillLeft :: a -> Int -> V.Vector a -> V.Vector a
 fillLeft z n v =
@@ -48,21 +51,15 @@ fillLeft z n v =
 getImmediateNeighbors :: Int -> V.Vector Int
 getImmediateNeighbors i = V.enumFromTo (i - 1) (i + 1)
 
-numRule :: Int -> ((V.Vector Bool -> Bool) -> (Int -> V.Vector Int) -> a) -> a
+numRule :: Word8 -> ((V.Vector Bool -> Bool) -> (Int -> V.Vector Int) -> a) -> a
 numRule n k =
     k 
     -- the rule itself is represented as 8 bits, a zero or one for each
     -- possible value of the neighbors, which itself is viewed as a binary
     -- number by treating its values as bits
     (\neighbors ->
-        fillLeft False 8 (intToBoolVec n) V.! (7 - boolVecToInt neighbors))
+        fillLeft False 8 (integralToBoolVec n) V.! (7 - boolVecToInt neighbors))
     getImmediateNeighbors
-
-
-rule30 :: ((V.Vector Bool -> Bool) -> (Int -> V.Vector Int) -> a) -> a
-rule30 k = k update getNeighbors where
-    update v = let n = boolVecToInt v in n >=1 && n <= 4
-    getNeighbors = getImmediateNeighbors
 
 printSteps :: (V.Vector Bool -> Bool)
            -> (Int -> V.Vector Int)
@@ -75,13 +72,20 @@ printSteps update getNeighbors n a
       printSteps update getNeighbors (n-1)
       (stepAutomaton update getNeighbors a)
 
+-- using undefined for the starting index since it should never
+-- matter, we only do operations that operate on all indices via 'extend'
 middleCellOn :: Automaton
 middleCellOn = let middleIndex = cellArrayLen `div` 2
-                in Automaton $ store (== middleIndex) middleIndex
+                in Automaton $ store (== middleIndex) undefined
 
 lastCellOn :: Automaton
 lastCellOn = let lastIndex = cellArrayLen - 1
-              in Automaton $ store (== lastIndex) lastIndex
+              in Automaton $ store (== lastIndex) undefined
+
+randomCellsOn :: MonadRandom m => m Automaton
+randomCellsOn =
+    (\v -> Automaton $ StoreT (Identity $ CellArray v) undefined)
+    <$> V.replicateM cellArrayLen getRandom
 
 instance Functor CellArray where
     fmap f (CellArray v) = CellArray $ fmap f v
