9879e680b417be4b5a82f85a9da07a67507ed6b8
Robert Benson <rbenson@bandwidth.com>
2024-02-15 10:08:11 -0500

    fix derpbob to go line by line

--- src/DerpBob.hs
+++ src/DerpBob.hs
@@ -4,9 +4,10 @@ import Data.Char
 import Data.Text (Text)
 import qualified Data.Text as Text
 import qualified Data.Text.IO as Text
+import System.IO (isEOF)
 
 main :: IO ()
-main = Text.interact derpbob
+main = linewise derpbob
 
 derpbob :: Text -> Text
 derpbob = snd . Text.mapAccumL (\ b c -> (not b, swapCaseIf b c)) False
@@ -19,3 +20,10 @@ swapCase c
     | isLower c = toUpper c
     | isUpper c = toLower c
     | otherwise = c
+
+linewise :: (Text -> Text) -> IO ()
+linewise f = do
+    line <- Text.getLine
+    Text.putStrLn (f line)
+    stop <- isEOF
+    if stop then pure () else linewise f
