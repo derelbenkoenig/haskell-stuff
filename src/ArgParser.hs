b5761b4c43b70ab097511d1d1739f5dfa23c2b1f
Robert Benson <rbenson@bandwidth.com>
2024-03-01 16:31:07 -0500

    Derpbob program and arg parser

---
+++ src/ArgParser.hs
@@ -0,0 +1,161 @@
+module ArgParser (
+    ArgParser(..),
+    parseEither,
+    endOfArgs,
+    shortOpt,
+    longOpt,
+    shortLongOpt,
+    argument,
+    exact,
+    errMsg,
+    parseProgramArgs,
+    shortFlag,
+    longFlag,
+    shortLongFlag,
+
+    ArgReader(..),
+    autoReader,
+    module Control.Monad.Combinators
+) where
+
+import Control.Applicative
+import Control.Monad
+import Control.Monad.Combinators
+import Text.Read (readEither)
+import System.Environment (getArgs)
+
+newtype ArgParser a = ArgParser { runArgParser :: [String] -> ([String], Either String a) }
+
+-- just triple fmap since s -> (s, Either e a) is a composition of functors
+-- (->) s ∘ (,) s ∘ Either e
+instance Functor ArgParser where
+    fmap f = ArgParser . (fmap . fmap . fmap) f . runArgParser
+
+instance Applicative ArgParser where
+    pure a = ArgParser $ \args -> (args, Right a)
+    ArgParser f <*> ArgParser a = ArgParser $ \args -> let
+        (args', ef) = f args
+        in case ef of
+            Left e -> (args', Left e)
+            Right f' -> let
+                (args'', a') = a args'
+                in (args'', fmap f' a')
+
+instance Alternative ArgParser where
+    empty = ArgParser $ \args -> (args, Left "No parse")
+    ArgParser a <|> ArgParser b = ArgParser $ \args -> let
+        (args', ea) = a args
+        in case ea of
+            Left _ -> b args'
+            Right a' -> (args', Right a')
+
+instance Monad ArgParser where
+    ArgParser a >>= f = ArgParser $ \args -> let
+        (args', ea) = a args
+        in case ea of
+            Left e -> (args', Left e)
+            Right a' -> runArgParser (f a') args'
+
+instance MonadPlus ArgParser where
+    mzero = empty
+    mplus = (<|>)
+
+instance MonadFail ArgParser where
+    fail e = ArgParser $ \args -> (args, Left e)
+
+newtype ArgReader a = ArgReader { readArg :: String -> Either String a }
+
+instance Functor ArgReader where
+    fmap f (ArgReader r) = ArgReader $ fmap f . r
+
+instance Applicative ArgReader where
+    pure a = ArgReader $ const (Right a)
+    ArgReader f <*> ArgReader a = ArgReader $ \s -> f s <*> a s
+
+newtype EitherString a = EitherString { getEitherString :: Either String a }
+
+instance Functor EitherString where
+    fmap f (EitherString e) = EitherString (fmap f e)
+
+instance Applicative EitherString where
+    pure = EitherString . Right
+    EitherString f <*> EitherString a = EitherString (f <*> a)
+
+instance Alternative EitherString where
+    empty = EitherString (Left "No parse")
+    EitherString a <|> EitherString b = EitherString $ case a of
+        Left _ -> b
+        Right _ -> a
+
+instance Alternative ArgReader where
+    empty = ArgReader $ const (Left "No parse")
+    ArgReader a <|> ArgReader b = ArgReader $ \s ->
+        getEitherString $ (EitherString $ a s) <|> (EitherString $ b s)
+
+instance Monad ArgReader where
+    ArgReader a >>= f = ArgReader $ \s -> case a s of
+        Left e -> Left e
+        Right a' -> readArg (f a') s
+
+autoReader :: Read a => ArgReader a
+autoReader = ArgReader readEither
+
+parseProgramArgs :: ArgParser a -> IO a
+parseProgramArgs p = do
+    args <- getArgs
+    case parseEither p args of
+        Left e -> fail e
+        Right a -> pure a
+
+parseEither :: ArgParser a -> [String] -> Either String a
+parseEither = (snd .) . runArgParser
+
+endOfArgs :: ArgParser ()
+endOfArgs = ArgParser $ \args ->
+    if null args then (args, Right ()) else (args, Left "Extra arguments")
+
+shortFlag :: Char -> ArgParser ()
+shortFlag c = ArgParser $ \args -> case args of
+    ('-':c':[]):restArgs | c' == c -> (restArgs, Right ())
+    x -> (x, Left "No parse")
+
+longFlag :: String -> ArgParser ()
+longFlag s = ArgParser $ \args -> case args of
+    ('-':'-':opt):restArgs | opt == s -> (restArgs, Right ())
+    x -> (x, Left "No parse")
+
+shortLongFlag :: Char -> String -> ArgParser ()
+shortLongFlag c s = try (shortFlag c) <|> longFlag s
+
+shortOpt :: Char -> ArgReader a -> ArgParser a
+shortOpt c r = shortFlag c *> argument r
+
+longOpt :: String -> ArgReader a -> ArgParser a
+longOpt s r = longFlag s *> argument r
+
+shortLongOpt :: Char -> String -> ArgReader a -> ArgParser a
+shortLongOpt c s r = shortOpt c r <|> longOpt s r
+
+exact :: String -> ArgParser String
+exact s = ArgParser $ \args -> case args of
+    (arg1:restArgs) | arg1 == s -> (restArgs, Right arg1)
+    x -> (x, Left "No parse")
+
+argument :: ArgReader a -> ArgParser a
+argument r = ArgParser $ \args -> case args of
+    (arg1:restArgs) -> (restArgs, readArg r arg1)
+    x -> (x, Left "No parse")
+
+errMsg :: String -> ArgParser a -> ArgParser a
+errMsg e p = ArgParser $ \args -> let
+    (args', res) = runArgParser p args
+    in case res of
+        Left _ -> (args', Left e)
+        Right a -> (args', Right a)
+
+try :: ArgParser a -> ArgParser a
+try p = ArgParser $ \args -> let
+    (args', res) = runArgParser p args
+    in case res of
+        Left e -> (args, Left e)
+        Right a -> (args', Right a)
--- src/DerpBob.hs
+++ src/DerpBob.hs
@@ -1,19 +1,93 @@
 module Main where
 
+import ArgParser
+import Control.Monad.Random
 import Data.Char
+import Data.Monoid (Endo(..))
 import Data.Text (Text)
 import qualified Data.Text as Text
 import qualified Data.Text.IO as Text
 import System.IO (isEOF)
 
 main :: IO ()
-main = linewise derpbob
+main = do
+    mode <- parseProgramArgs parseArgs
+    accum <- accumForMode mode
+    linewise accum
 
-derpbob :: Text -> Text
-derpbob = snd . Text.mapAccumL (\ b c -> (not b, swapCaseIf b c)) False
+data DerpBobMode = Alternating | Randomized !Double
+    deriving (Eq, Show)
 
-swapCaseIf :: Bool -> Char -> Char
-swapCaseIf cond c = if cond then swapCase c else c
+toAlternating :: Endo DerpBobMode
+toAlternating = Endo $ const Alternating
+
+toDefaultRandom :: Endo DerpBobMode
+toDefaultRandom = Endo $ \mode -> case mode of
+    Randomized p -> Randomized p
+    Alternating -> defaultRandom
+
+withProbability :: Double -> Endo DerpBobMode
+withProbability p = Endo $ \mode -> case mode of
+    Randomized _ -> Randomized p
+    Alternating -> Randomized p
+
+parseMode :: ArgParser (Endo DerpBobMode)
+parseMode =
+    toAlternating <$ shortLongFlag 'a' "alternating"
+    <|> toDefaultRandom <$ shortLongFlag 'r' "random"
+    <|> withProbability <$> (shortLongOpt 'p' "probability" autoReader)
+
+parseArgs :: ArgParser DerpBobMode
+parseArgs = (`appEndo` Alternating) <$> go where
+    go = mempty <$ endOfArgs <|> flip (<>) <$> parseMode <*> go
+
+defaultRandom :: DerpBobMode
+defaultRandom = Randomized 0.9
+
+accumForMode :: DerpBobMode -> IO (Text -> Text)
+accumForMode Alternating = pure $ derpbob altCase False
+accumForMode (Randomized d) = do
+    g <- getStdGen
+    pure $ derpbob (randomAccum (randomFlip d)) (RandState g Lower)
+
+type TextAccumFunc s = s -> Char -> (s, Char)
+type RandGenFunc g a = g -> (a, g)
+
+derpbob :: TextAccumFunc s -> s -> Text -> Text
+derpbob f z = snd . Text.mapAccumL f z
+
+altCase :: TextAccumFunc Bool
+altCase b c = (not b, changeIf b swapCase c)
+
+randomFlip :: RandomGen g => Double -> g -> (Bool, g)
+randomFlip prob gen = let
+    ~(randVal, newGen) = random gen
+    in (randVal < prob, newGen)
+
+data CharCase = Upper | Lower
+
+flipCase :: CharCase -> CharCase
+flipCase Upper = Lower
+flipCase Lower = Upper
+
+caseChar :: CharCase -> Char -> Char
+caseChar Upper = toUpper
+caseChar Lower = toLower
+
+data RandState g = RandState !g !CharCase
+
+randomAccum :: RandGenFunc g Bool -> TextAccumFunc (RandState g)
+randomAccum f (RandState gen casing) c = let
+    ~(doFlip, newG) = f gen
+    newCase = if doFlip then flipCase casing else casing
+    newState = RandState newG newCase
+    in (newState, caseChar casing c)
+
+changeIf :: Bool -> (a -> a) -> a -> a
+changeIf cond f x = if cond then f x else x
+
+changeIfM :: Monad m => Bool -> (a -> m a) -> a -> m a
+changeIfM cond f x = if cond then f x else pure x
 
 swapCase :: Char -> Char
 swapCase c
@@ -24,6 +98,7 @@ swapCase c
 linewise :: (Text -> Text) -> IO ()
 linewise f = do
     line <- Text.getLine
-    Text.putStrLn (f line)
+    let lineOut = f line
+    Text.putStrLn lineOut
     stop <- isEOF
     if stop then pure () else linewise f
