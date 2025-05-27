0c6c457a0b9697ab948aef50a2e14bf00d4e0410
Robert Benson <derelbenkoenig@gmail.com>
2025-05-27 00:23:30 -0400

    give stuff types

--- src/EDO.hs
+++ src/EDO.hs
@@ -2,15 +2,36 @@
 {-# OPTIONS_GHC -Wno-missing-signatures #-}
 module EDO where
 
-
+log2 :: Floating a => a -> a
 log2 = logBase 2
-ratioToSteps edo ratio = edo * log2 ratio
-stepsToRatio edo steps = 2 ** (steps / edo)
+
+roundN :: (RealFrac a, Integral b) => b -> a -> a
+roundN n = (/ fac) . fromIntegral . round . (* fac) where
+    fac = 10 ^ n
+
+ratioToSteps :: (Integral edo, Floating r) => edo -> r -> r
+ratioToSteps edo ratio = fromIntegral edo * log2 ratio
+
+stepsToRatio :: (Integral edo, Floating a) => edo -> a -> a
+stepsToRatio edo steps = 2 ** (steps / fromIntegral edo)
+
+nearestEdo :: (Integral edo, Floating a, RealFrac a) => edo -> a -> edo
 nearestEdo edo ratio = round (ratioToSteps edo ratio)
-centError edo ratio = round ((approximation - exactSteps) / edo * 1200)
+
+centError :: (Integral edo, Floating r, RealFrac r) => edo -> r -> r
+centError edo ratio = roundN 2 ((approximation - exactSteps) / fromIntegral edo * 1200)
     where
     exactSteps = ratioToSteps edo ratio
     approximation = fromInteger (round exactSteps)
+
+majorRatios :: [Rational]
 majorRatios = [9/8, 5/4, 4/3, 3/2, 5/3, 15/8]
-majorScale edo = map (nearestEdo edo) majorRatios
-majorError edo = map (centError edo) majorRatios
+
+majorScale :: (Integral edo, Floating a) => edo -> [a]
+majorScale edo = map (fromIntegral . nearestEdo edo) (fromRational <$> majorRatios)
+
+majorError :: (Integral edo, Floating r, RealFrac r) => edo -> [r]
+majorError edo = map (centError edo) (fromRational <$> majorRatios)
+
+sse :: (Foldable t, Functor t, Floating a) => t a -> a
+sse = sqrt . sum . fmap (^ 2)
