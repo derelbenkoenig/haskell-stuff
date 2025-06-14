db328a34ffec3fec58c6b571b49816f9a1f9ff89
Robert Benson <derelbenkoenig@gmail.com>
2025-06-14 00:00:04 -0400

    Make dice its own project

--- dice/app/Main.hs
+++ dice/app/Main.hs
@@ -1,6 +1,27 @@
 module Main (main) where
 
-import Lib
+import Dice
+import Dice.Parsing
+import Data.Bifunctor
+import Exinst (Some1, withSome1)
+import Data.Text (pack)
+
+import Text.Megaparsec (parse, errorBundlePretty)
 
 main :: IO ()
-main = someFunc
+main = do
+    eDice <- readDiceRoll
+    case eDice of
+        Left e -> putStrLn e
+        Right someDice -> withSome1 someDice $ \ dice -> do
+            (notation, _, display, total) <- resultDisplayAndTotal dice
+            putStr notation
+            putStr ": "
+            putStr display
+            putStr " = "
+            putStr (show total)
+            putStrLn ""
+    main
+
+readDiceRoll :: IO (Either String (Some1 DiceRoll))
+readDiceRoll = first errorBundlePretty . parse diceRoll "" . pack <$> getLine
